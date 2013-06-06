ac7fa3ac7b262d2feb9d528d3cc0a1a8199e42bb
Noah Malik <nhmalik@umail.ucsb.edu>
2013-06-05 23:25:51 -0700

    Added stream of audio input inside the thread. No FFT or user notification implimented yet.

--- src/edu/ucsb/cs56/projects/music/guitar_tuner/Tune.java
+++ src/edu/ucsb/cs56/projects/music/guitar_tuner/Tune.java
@@ -11,22 +11,60 @@ public class Tune extends Thread {
 	private volatile boolean stopRequested = false;
 
 	// Default Constructor
-	Tune() {this.pitch = 0; this.msg = new JLabel();}
+	Tune() {this.pitch = 0; this.msg = new JLabel(); line = null;}
 
 	// Constructor to set pitch
 	Tune(int pitch, JLabel msg) {
 		this.pitch = pitch;
 		this.msg = msg;
+		line = null;
 		stopRequested = false;
+	}
+
+	static byte buffer[];
+	static TargetDataLine line;
+
+	static {
+		try {
+			final AudioFormat format = presetFormat();
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+			line = (TargetDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start();
+			int bufferSize = (int) format.getSampleRate()*format.getFrameSize();
+			buffer = new byte[bufferSize];
+		} catch(LineUnavailableException e) {
+			System.err.println("Line Unavailable: " + e);
+			System.exit(-1);
+		}
 	}
 
 	// Override run method of Thread
 	public void run(){
-		while(!stopRequested) {
-			// do stuff
 			msg.setText("Tuning...");
+			while(!stopRequested) {
+				line.read(buffer, 0, buffer.length);
 
-		}
+				/* Now FFT with Minim package
+					Declare a MAudioBuffer with the buffer array we have
+					Declare an FFT with the MAudioBuffer
+					Find amp of pitch
+					If pitch isn't strong, need to tune
+					Find which way to tune
+					Display Up/Down/Done
+
+				*/
+
+			}
+	}
+
+	private static AudioFormat presetFormat() {
+	float sampleRate = 8000;
+    int sampleSizeInBits = 8;
+    int channels = 1;
+    boolean signed = true;
+    boolean bigEndian = true;
+    return new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
 	}
 
 	public void requestStop() {
